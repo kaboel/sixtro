a2a1466aa76c0748a2dab5faeab4206e2f368649
yudistiraardi <mail.ardix@gmail.com>
2023-09-05 19:07:05 +0700

    belajar let & const

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // VIDEO 3 - LET AND CONST DECLARATION
 
-function testVar(){
+/*
+function xVar(){ // variable gak bisa 
     var x = 10;
     if(true){
         var x = 100;
@@ -8,3 +9,29 @@ function testVar(){
     }
     console.log(x);
 }
+
+function xLet(){
+    let x = 10;
+    if(true){
+        let x = 100;
+        console.log(x);
+    }
+    console.log(x);
+}
+
+// let i = 10;
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+// console.log(i);
+*/
+
+const colors = [];
+
+colors.push('purple'); // sejujurnya malah bingung apa itu push tp gpp nanti gw pelajari
+colors.push('black');
+
+// colors = 'pink';
+
+console.log(colors);
